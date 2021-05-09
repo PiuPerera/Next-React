5460cdba2293c9eb8558413133c242b1abeedacf
Udara Sachinthana <U4dara@gmail.com>
2021-05-09 14:27:37 +0530

    API connected

--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -1,16 +1,18 @@
 import React, {Component} from "react";
 
 class Pizza extends Component{
-state = {};
+    state = {};
 
     render(){
         return(
                 <div className="card" style={{width: "16rem"}}>
-                    <img className="card-img-top" src="https://adminsc.pizzahut.lk//images/mainmenu/e3205655-8b4b-4f9c-9279-4e25c257e032.jpg" alt=""/>
+                    <img className="card-img-top" src={this.props.pizza.imgUrl} alt=""/>
                     <div className="card-body">
-                        <h5 className="card-title">Chicken Sausage Meat With Nai Miris</h5>
-                        <p className="card-text">A fiery blend of kotchchi chicken sausage meat with nai miris, set upon an all new kotchchi sauce base topped with onion, capsicum and a double layer of mozzarella cheese.</p>
-                        <button href="#" className="btn btn-primary">Go somewhere</button>
+                        <h5 className="card-title">{this.props.pizza.name}</h5>
+                        <p className="card-text">{this.props.pizza.details}</p>
+                        {/* <ul>{this.props.pizza.ingredients.map((pizza, index) => <li key={index}>{pizza}</li>)}</ul> */}
+                        <h5 style={{color:"red"}}>Rs.{this.props.pizza.price}/=</h5>
+                        <button href="#" className="btn btn-primary">View More</button>
                     </div>
                 </div>
         );
--- src/components/pizzaList.jsx
+++ src/components/pizzaList.jsx
@@ -1,21 +1,41 @@
 import React, {Component} from "react";
-import Pizza from "./pizza"
+import axios from "axios";
+import Pizza from "./pizza";
 
 class PizzaList extends Component{
+    state = {
+        pizza_List : [],
+    };
+
     render(){
         return <div className="container">
             <div className="row">
-                <div className="col">
-                    <Pizza/>
-                </div>
-                <div className="col">
-                    <Pizza/>
-                </div>
-                <div className="col">
-                    <Pizza/>
-                </div>
+                {
+                    this.state.pizza_List.map((pizza) => (
+                        <div key={pizza.id} className="col">
+                            <Pizza key={pizza.id} pizza={pizza}/>
+                        </div> 
+                    ))
+                }
             </div>
         </div>
+    }
+
+    async componentDidMount(){
+        const {data} =  await axios.get("http://localhost:5000/api/pizzahub/products");
+        let pizzas = data.map((pizza) => {
+            return {
+                id: pizza._id,
+                imgUrl: pizza.imageUrl,
+                name: pizza.name,
+                price: pizza.price,
+                availability: pizza.availability,
+                size: pizza.size,
+                ingredients: pizza.ingredients,
+                details: pizza.details
+            };
+        });
+        this.setState({pizza_List: pizzas});
     }
 }
 
