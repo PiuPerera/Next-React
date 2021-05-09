963b605706f706aa9f64186a9a827f2c6c462125
Udara Sachinthana <U4dara@gmail.com>
2021-05-09 19:59:38 +0530

    Update pizza.jsx

--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -10,7 +10,7 @@ class Pizza extends Component{
                     <div className="card-body">
                         <h5 className="card-title">{this.props.pizza.name}</h5>
                         <p className="card-text">{this.props.pizza.details}</p>
-                        {/* <ul>{this.props.pizza.ingredients.map((pizza, index) => <li key={index}>{pizza}</li>)}</ul> */}
+                        <ul>{this.props.pizza.ingredients.map((pizza, index) => <li key={index}>{pizza}</li>)}</ul>
                         <h5 style={{color:"red"}}>Rs.{this.props.pizza.price}/=</h5>
                         <button href="#" className="btn btn-primary">View More</button>
                     </div>
